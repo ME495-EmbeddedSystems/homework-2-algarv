8a4c5a0f1a19487a28a461e68d6ec573287ebe9d
Anna Garverick <annagarverick2022@u.northwestern.edu>
2021-10-11 11:28:39 -0500

    Setting up package and workspace

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/algarverick/ME_495/homework_2/homework-2-algarv/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/algarverick/Pen/devel;/opt/ros/noetic'.split(';') if '/home/algarverick/Pen/devel;/opt/ros/noetic' != '' else []
--- build/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest
-# Build directory: /home/algarverick/ME_495/homework_2/homework-2-algarv/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
