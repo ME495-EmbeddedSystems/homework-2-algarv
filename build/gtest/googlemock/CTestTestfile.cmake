e9a32a4141273215b5f3143173e915f4aa62a9d2
Anna Garverick <annagarverick2022@u.northwestern.edu>
2021-10-11 11:17:17 -0500

    Setting up package and workspace

---
+++ build/gtest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest/googlemock
+# Build directory: /home/algarverick/ME_495/homework_2/homework-2-algarv/build/gtest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
