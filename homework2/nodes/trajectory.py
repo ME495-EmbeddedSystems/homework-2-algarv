be9c62d99b1e1387481664c98065386e08fe2198
Anna Garverick <annagarverick2022@u.northwestern.edu>
2021-10-13 20:39:43 -0500

    Publishing twist value and moving turtle

--- homework2/nodes/trajectory.py
+++ homework2/nodes/trajectory.py
@@ -2,19 +2,28 @@
 import rospy
 import numpy as np
 from geometry_msgs.msg import Twist,Vector3
-#from turtle_control.msg import TurtleVelocity
+from turtlesim.msg import Pose
 
-def main():
-    pub = rospy.Publisher('turtle1/cmd_vel',Twist,queue_size = 500)
+
+def callback(data):
+    pub = rospy.Publisher('turtle1/cmd_vel',Twist,queue_size = 10)
+    t = rospy.get_time()
     parameters = rospy.get_param("/Parameters")
     W = parameters[0]
     H = parameters[1]
     T = parameters[2]
-    t = rospy.get_time()
-    twist_value = Twist(Vector3(x=(W/2)*np.sin(2*np.pi*t)/T,y=(H/2)*np.sin((4*np.pi*t)/T),z=0),Vector3(x=0,y=0,z=0))
+    twist_value = Twist(Vector3(x=(W*np.pi/T)*np.cos(2*np.pi*t/T),y=(2*H*np.pi*t/T)*np.cos((4*np.pi*t)/T),z=0),Vector3(x=0,y=0,z=0))
     pub.publish(twist_value)
+
+def main():
+    parameters = rospy.get_param("/Parameters")
+    rospy.Subscriber("turtle1/pose",Pose,callback)
+    W = parameters[0]
+    H = parameters[1]
+    T = parameters[2]
 
 if __name__ == '__main__':
     rospy.init_node('trajectory')
+    rospy.Rate(500)
     main()
     rospy.spin()
