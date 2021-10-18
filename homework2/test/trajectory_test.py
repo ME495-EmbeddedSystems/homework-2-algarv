f911aea71b9ac2d06ee22cef2a13bff923c09955
Anna Garverick <annagarverick2022@u.northwestern.edu>
2021-10-17 20:36:18 -0500

    I have a horizontal pole

--- homework2/test/trajectory_test.py
+++ homework2/test/trajectory_test.py
@@ -8,6 +8,9 @@ class TestCase(unittest.TestCase):
         traj = trajectory(0,0,1)
         self.assertEquals(traj.linear_velocity(0),0)
 
+    def test_something_else(self):
+        self.assertEquals(0,0)
+
 if __name__ == "__main__":
     import rosunit
     rosunit.unitrun(homework2, 'Trajectory_Test', TestCase)
