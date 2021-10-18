b8dae828bf5db0a6a0a2a258fcf7c5ec5f2e9438
Anna Garverick <annagarverick2022@u.northwestern.edu>
2021-10-18 18:27:49 -0500

    Restructured calc_trajectory to faciliate testing, added all t=0 tests

--- homework2/src/homework2/calc_trajectory.py
+++ homework2/src/homework2/calc_trajectory.py
@@ -11,28 +11,41 @@ class trajectory():
         self.W = W
         self.H = H
         self.T = T 
-
         self.t = symbols('t')
 
-        x = (W/2)*sin((2*pi*self.t)/T)
-        y = (H/2)*sin((4*pi*self.t)/T)
+    def calc_x(self):
+        self.x = (self.W/2)*sin((2*pi*self.t)/self.T)
+        self.x_dot = self.x.diff(self.t)
+        self.x_ddot = self.x_dot.diff(self.t)
+
+        return self.x, self.x_dot, self.x_ddot
         
-        self.x_dot = x.diff(self.t)
-        self.y_dot = y.diff(self.t)
-    
+    def calc_y(self):
+        self.y = (self.H/2)*sin((4*pi*self.t)/self.T)
+        self.y_dot = self.y.diff(self.t)
+        self.y_ddot = self.y_dot.diff(self.t)
+
+        return self.y, self.y_dot, self.y_ddot
+
     def theta0(self):
-        theta0_eq = sqrt(self.x_dot**2 + self.x_dot**2)
+        self.x, self.x_dot, self.x_ddot = trajectory(self.W,self.H,self.T).calc_x()
+        self.y, self.y_dot, self.y_ddot = trajectory(self.W,self.H,self.T).calc_y()
+        theta0_eq = sqrt(self.x_dot**2 + self.y_dot**2)
         theta0_func = sym.lambdify(self.t,theta0_eq,modules=sym)
         theta0 = theta0_func(0)
         return theta0
 
     def linear_velocity(self,time):
+        self.x, self.x_dot, self.x_ddot = trajectory(self.W,self.H,self.T).calc_x()
+        self.y, self.y_dot, self.y_ddot = trajectory(self.W,self.H,self.T).calc_y()
         v_eq = sqrt(self.x_dot**2 + self.y_dot**2) 
         v_func = sym.lambdify(self.t,v_eq,modules=sym)
         self.v = v_func(time)
         return self.v
     
     def angular_velocity(self,time):
+        self.x, self.x_dot, self.x_ddot = trajectory(self.W,self.H,self.T).calc_x()
+        self.y, self.y_dot, self.y_ddot = trajectory(self.W,self.H,self.T).calc_y()
         theta = sym.atan(self.y_dot/self.x_dot)
         w_eq = theta.diff(self.t)
         w_func = sym.lambdify(self.t,w_eq,modules=sym)
--- homework2/test/trajectory_test.py
+++ homework2/test/trajectory_test.py
@@ -1,15 +1,60 @@
 #!/usr/bin/env python
 
 import unittest
+import sympy as sym
 from homework2.calc_trajectory import trajectory
 
 class TestCase(unittest.TestCase):
-    def test_something(self):
-        traj = trajectory(0,0,1)
-        self.assertEquals(traj.linear_velocity(0),0)
+    def test_x(self):
+        W = 0
+        H = 0
+        T = 1
+        self.t = sym.symbols('t')
+        self.traj = trajectory(W,H,T)
+        self.x, self.x_dot, self.x_ddot = self.traj.calc_x()
 
-    def test_something_else(self):
-        self.assertEquals(0,0)
+        x = sym.lambdify(self.t,self.x)
+        self.assertEquals(x(0),0)
+
+        x_dot = sym.lambdify(self.t,self.x_dot)
+        self.assertEquals(x_dot(0),0)
+
+        x_ddot = sym.lambdify(self.t,self.x_ddot)
+        self.assertEquals(x_dot(0),0)
+    
+    def test_y(self):
+        W = 0
+        H = 0
+        T = 1
+        self.t = sym.symbols('t')
+        self.traj = trajectory(W,H,T)
+        self.y, self.y_dot, self.y_ddot = self.traj.calc_y()
+
+        y = sym.lambdify(self.t,self.y)
+        self.assertEquals(y(0),0)
+
+        y_dot = sym.lambdify(self.t,self.y_dot)
+        self.assertEquals(y_dot(0),0)
+
+        y_ddot = sym.lambdify(self.t,self.y_ddot)
+        self.assertEquals(y_ddot(0),0)
+
+    def test_linear_velocity(self):
+        W = 0
+        H = 0
+        T = 1
+        self.t = sym.symbols('t')
+        self.traj = trajectory(W,H,T)
+        self.assertEquals(self.traj.linear_velocity(0),0)
+        
+    def test_angular_velocity(self):
+        W = 0
+        H = 0
+        T = 1
+        self.t = sym.symbols('t')
+        self.traj = trajectory(W,H,T)
+        self.assertEquals(self.traj.angular_velocity(0),0)
+
 
 if __name__ == "__main__":
     import rosunit
